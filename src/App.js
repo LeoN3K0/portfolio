94251e2796ec099f6ea4fbcc98d0e500bccb91ff
Erich Malberg <emalberg@live.com>
2023-05-18 12:43:17 -0600

    changing particles

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import About from './Components/About';
 import Work from './Components/Work';
 import Skills from './Components/Skills';
 import Contact from './Components/Contact';
+import ParticlesBg from "particles-bg";
 
 
 function App() {
   return (
     <div>
+    <ParticlesBg color="#F5F5F5" type="lines" bg={true} /> 
     <Nav/>
     <Home/>
     <About/>
