7d3f24cb1e7dd04cd2c755cb5bcc14782aaa4bce
Erich Malberg <emalberg@live.com>
2023-05-18 12:19:19 -0600

    Adding link to resume

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -57,7 +57,7 @@ const Nav = () => {
                         </a>
                     </li>
                     <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-gray-600">
-                        <a className="flex justify-between items-center w-full" href="https://jmp.sh/ylz0VXIG" target="_blank">
+                        <a className="flex justify-between items-center w-full" href="https://jmp.sh/RfLMXxQR" target="_blank">
                             Resume <BsFillPersonLinesFill size={30}/>
                         </a>
                     </li>
