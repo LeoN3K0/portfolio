21a63071b3f4bac0c4197bb3dcaf0a00e63a4a0b
Erich Malberg <emalberg@live.com>
2023-05-18 13:28:13 -0600

    changing particles

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,7 +5,8 @@ import {Link} from "react-scroll";
 
 const Home = () => {
     return (
-            <div name='home' className="w-full  h-screen bg-black">
+            <div name='home' className="w-full  h-screen">
+                <ParticlesBg color="#F5F5F5" type="lines" bg={true} className="h-screen" /> 
                 {/*Container*/}
                 <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
                     <p className="text-[#3ebea8]">Hello, my name is</p>
