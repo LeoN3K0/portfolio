b0548faada5ade6f4f6d50eba162625086b2a253
Erich Malberg <emalberg@live.com>
2023-05-23 11:27:30 -0600

    adding more work info

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -1,5 +1,6 @@
 import React from "react";
 import robotFriends from '../Assets/robots.jpg';
+import faceDetection from '../Assets/Face.png';
 
 const Work = () => {
     return(
@@ -23,6 +24,23 @@ const Work = () => {
                                     <button className="text-center rounded-lg px-4 py-3 m-2 bg-[#F5F5F5] text-[#1e1926] font-bold text-lg">Demo</button>
                                 </a>
                                 <a href="https://github.com/LeoN3K0/robofriends" target="_blank">
+                                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-[#F5F5F5] text-[#1e1926] font-bold text-lg">Code</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div style={{backgroundImage: `url(${faceDetection})`}} 
+                    className="shadow-lg shadow-[#F5F5F5] group container rounded-md flex justify-center items-center mx-auto content-div">
+                        {/* Hover Effects */}
+                        <div className="opacity-0 group-hover:opacity-100">
+                            <span className="text-2xl font-bold text-[#F5F5F5] tracking-wider">
+                                Robo Friends
+                            </span>
+                            <div className="pt-8 text-center">
+                                <a href="https://leon3k0.github.io/smartimg/" target="_blank">
+                                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-[#F5F5F5] text-[#1e1926] font-bold text-lg">Demo</button>
+                                </a>
+                                <a href="https://github.com/LeoN3K0/smartimg" target="_blank">
                                     <button className="text-center rounded-lg px-4 py-3 m-2 bg-[#F5F5F5] text-[#1e1926] font-bold text-lg">Code</button>
                                 </a>
                             </div>
